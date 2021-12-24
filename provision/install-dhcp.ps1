61a5cf1fea9b6a47b87b8613ac11a3718ae53d11
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2021-12-24 16:29:54 +0100

    updates

--- provision/install-dhcp.ps1
+++ provision/install-dhcp.ps1
@@ -19,3 +19,12 @@ Set-ItemProperty -Path "registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerMa
 Add-DhcpServerv4Scope -name "main" -StartRange "192.168.56.101" -EndRange "192.168.56.200" -SubnetMask "255.255.255.0" -State Active
 
 Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -DnsServer "192.168.56.2" -DnsDomain "mylab.com"
+Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 066 -Value "192.168.56.3"
+Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x86\wdsnbp.com" # für UEFI x64
+# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x86\wdsnbp.com" für x86
+# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x64\wdsnbp.com" für x64
+
+copy-item "C:\Windows\System32\RemInst\boot\x64\wdsmgfw.efi" "C:\remInstall\Boot\x64\wdsmgfw.efi"
+
+#https://community.spiceworks.com/topic/2150005-pxe-booting-uefi-dell-latitude-7404?page=1#entry-7853547
+#https://rakhesh.com/powershell/adding-dhcp-scope-options-via-powershell/
