9850b7524b4e74f54cf0b4bdc2c56f32ee6b3060
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2021-12-24 11:25:09 +0100

    changes

--- provision/Language/_set-language-german.ps1
+++ provision/Language/_set-language-german.ps1
@@ -1,46 +1,83 @@
-Write-Host -ForegroundColor Blue "###################################"
-Write-Host -ForegroundColor Blue "# >>>> start set-language-german   "
-Write-Host -ForegroundColor Blue "###################################"
+# from http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc
 
-$LANGUAGEFILE = "C:\vagrant\provision\Language\Microsoft-Windows-Server-Language-Pack_x64_de-de.cab"
-$LANGUAGE = "de-ch"
-$LOCAL = "de-ch"
+# notice: A chocolatey cinst PSWindowsUpdate will also do the trick
 
-# check language file
-if (Test-Path "$LANGUAGEFILE") {
-
-  DISM /Online /Add-Package /PackagePath:$LANGUAGEFILE 
-
-  # set primary language
-  Write-Host -ForegroundColor Blue "###################################"
-  Write-Host -ForegroundColor Blue "# >>>> set primary language        "
-  Write-Host -ForegroundColor Blue "###################################"
-  Set-WinUILanguageOverride $LANGUAGE
-
-  # set locale
-  Write-Host -ForegroundColor Blue "###################################"
-  Write-Host -ForegroundColor Blue "# >>>> set local                   "
-  Write-Host -ForegroundColor Blue "###################################"
-  Set-WinSystemLocale $LOCAL
-  
-  # set display language
-  Write-Host -ForegroundColor Blue "###################################"
-  Write-Host -ForegroundColor Blue "# >>>> set display language       "
-  Write-Host -ForegroundColor Blue "###################################"
-  Set-WinUserLanguageList $LOCAL -Force
-
-  # set timezone
-  Write-Host -ForegroundColor Blue "###################################"
-  Write-Host -ForegroundColor Blue "# >>>> set time zone               "
-  Write-Host -ForegroundColor Blue "###################################"
-  Set-TimeZone -Id "Central European Standard Time"
-
-} else {
-    Write-Host "Language file $LANGUAGEFILE doesn't exists"
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item)
+}
 }
 
-Write-Host -ForegroundColor Blue "###################################"
-Write-Host -ForegroundColor Blue "# >>>> end set-language-german     "
-Write-Host -ForegroundColor Blue "###################################"
+$Userprofile = get-content env:userprofile
 
+if ((Test-Path -Path "$Userprofile\Documents\WindowsPowerShell\Modules\PSWindowsUpdate") -ne $True)
+{
+  $DownloadUrl = 'http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/28/PSWindowsUpdate.zip'
+  $ZipFile = 'c:\vagrant\resources\PSWindowsUpdate.zip'
+
+  "Downloading PSWindowsUpdate.zip"
+  (New-Object Net.WebClient).DownloadFile("$DownloadUrl","$ZipFile")
+
+  if ((Test-Path -Path "$Userprofile\Documents\WindowsPowerShell\Modules") -ne $True)
+  {
+    New-Item -Path "$Userprofile\Documents\WindowsPowerShell\Modules" -ItemType directory
+  }
+  "Exctracting zip to $Userprofile\Documents\WindowsPowerShell\Modules"
+  Expand-ZIPFile -File $ZipFile -Destination "$Userprofile\Documents\WindowsPowerShell\Modules"
+}
+
+Import-Module PSWindowsUpdate
+
+# "List all available Windows Updates"
+# Get-WUList -MicrosoftUpdate 
+
+"Install some Language Packs"
+get-wuinstall -Title "Bulgarian Language Pack" -AcceptAll
+get-wuinstall -Title "Danish Language Pack" -AcceptAll
+get-wuinstall -Title "German Language Pack" -AcceptAll
+get-wuinstall -Title "Spanish Language Pack" -AcceptAll
+get-wuinstall -Title "French Language Pack" -AcceptAll
+get-wuinstall -Title "Italian Language Pack" -AcceptAll
+get-wuinstall -Title "Dutch Language Pack" -AcceptAll
+get-wuinstall -Title "Polish Language Pack" -AcceptAll
+get-wuinstall -Title "Romanian Language Pack" -AcceptAll
+
+# Arabic Language Pack
+# Bulgarian Language Pack
+# Chinese (Simplified) Language Pack
+# Chinese (Traditional) Language Pack
+# Croatian Language Pack
+# Czech Language Pack
+# Danish Language Pack
+# Dutch Language Pack
+# Estonian Language Pack
+# Finnish Language Pack
+# French Language Pack
+# German Language Pack
+# Greek Language Pack
+# Hebrew Language Pack
+# Hungarian Language Pack
+# Italian Language Pack
+# Japanese Language Pack
+# Korean Language Pack
+# Latvian Language Pack
+# Lithuanian Language Pack
+# Norwegian Language Pack
+# Polish Language Pack
+# Portuguese (Brazil) Language Pack
+# Portuguese (Portugal) Language Pack
+# Romanian Language Pack
+# Russian Language Pack
+# Serbian (Latin) Language Pack
+# Slovak Language Pack
+# Slovenian Language Pack
+# Spanish Language Pack
+# Swedish Language Pack
+# Thai Language Pack
+# Turkish Language Pack
+# Ukrainian Language Pack
 
