0ee789e0c44b8ca423e7a2b6aa801a58a66ec5a3
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2022-02-08 16:54:31 +0100

    Update install-dhcp.ps1

--- provision/install-dhcp.ps1
+++ provision/install-dhcp.ps1
@@ -4,25 +4,25 @@ Restart-Service dhcpserver
 start-sleep -Seconds 3
 
 #Todo Doublehop scenerio which causes this to fail
-#Add-DhcpServerInDC
+Add-DhcpServerInDC
 
 Set-ItemProperty -Path "registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12" -Name ConfigurationState -Value 2
 
-# [string]$userName = "mylab.com\vagrant"
-# [string]$userPassword = "vagrant"
-# [securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
+#[string]$userName = "mylab.com\Administrator"
+#[string]$userPassword = "P@ssw0rd"
+#[securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
 
-# [pscredential]$credOject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
+#[pscredential]$credOject = New-Object System.Management.Automation.PSCredential ($userName, #$secStringPassword)
 
-# Set-DhcpServerDnsCredential -Credential $credOject -ComputerName "mdt01.mylab.com"
+#Set-DhcpServerDnsCredential -Credential $credOject -ComputerName "mdt01.mylab.com"
 
 Add-DhcpServerv4Scope -name "main" -StartRange "192.168.56.101" -EndRange "192.168.56.200" -SubnetMask "255.255.255.0" -State Active
 
 Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -DnsServer "192.168.56.2" -DnsDomain "mylab.com"
-Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 066 -Value "192.168.56.3"
-Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x86\wdsnbp.com" # für UEFI x64
-# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x86\wdsnbp.com" für x86
-# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x64\wdsnbp.com" für x64
+# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 066 -Value "192.168.56.3"
+# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x86\wdsnbp.com" # für UEFI x64
+# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x86\wdsnbp.com" # für x86
+# Set-DhcpServerv4OptionValue -ScopeId "192.168.56.0" -OptionId 067 -Value "boot\x64\wdsnbp.com" #für x64
 
 copy-item "C:\Windows\System32\RemInst\boot\x64\wdsmgfw.efi" "C:\remInstall\Boot\x64\wdsmgfw.efi"
 
